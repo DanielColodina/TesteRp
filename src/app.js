9dd58f7aba3fbbae2d3c5bd188f5b08b2e0c670c
DanielVegano <danielbtcethe22@gmail.com>
2026-01-31 20:52:12 -0300

    TENTATIVAS

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ app.use(session({
 // -------- RATE LIMIT --------
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 17, // Máximo 5 tentativas
+  max: 50, // Máximo 5 tentativas
   message: 'Muitas tentativas de login. Tente novamente em 15 minutos.',
   standardHeaders: true,
   legacyHeaders: false,
